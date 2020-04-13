7b6514c287fc09f6ac9643453803bbd0277e4de1
Igor Samohvalov <sandypandaadv@outlook.com>
2020-04-13 15:53:07 +0300

    fixes v1.01

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
             <header>
                 <a href='#map' onClick={this.props.onClickMap}>Карта</a>
                 <a href='#profile' onClick={this.props.onClickProfile}>Профиль</a>
-                <a href='#' onClick={this.props.onClickExit, this.props.onLoginScreen}>Выйти</a>
+                <a href='#' onClick={this.props.onLoginScreen}>Выйти</a>
             </header>
         )
     }
